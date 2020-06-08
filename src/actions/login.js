0228fe1fa10abd73b2c63ddcdbcbca39d81154c2
Oskar Georgsson <oskar.georgsson@gmail.com>
2020-06-08 18:04:37 +0200

    Added method to signout

--- src/actions/login.js
+++ src/actions/login.js
@@ -2,7 +2,8 @@ import ApiUrlFactory from "../factories/ApiUrlFactory";
 
 import {
     SET_IS_SIGNED_IN,
-    SESSION_STORAGE_SET_ITEM
+    SESSION_STORAGE_SET_ITEM,
+    SESSION_STORAGE_REMOVE_ITEM
 } from "../constants/actionTypes";
 import { apiAction } from "./api";
 import {
@@ -20,12 +21,12 @@ export const signin = (username, password, onSuccess) => {
     });
 };
 
-export const signout = userKey => {
+export const signout = () => {
     return apiAction({
-        url: ApiUrlFactory.login.signout(userKey),
+        url: ApiUrlFactory.login.signout(),
         method: "DELETE",
         label: "signout",
-        onSuccess: () => setIsSignedin(false),
+        onSuccess: removeUserKeyAndSetSignedOut,
         onFailure: () => console.log("Error when signing out")
     })
 };
@@ -37,6 +38,16 @@ export const saveUserKeyAndSetSignedIn = userKey => {
             sessionKey: AGIFBOOKING_USER_KEY,
             data: userKey,
             onSuccess: () => setIsSignedin(true)
+        }
+    };
+};
+
+export const removeUserKeyAndSetSignedOut = () => {
+    return {
+        type: SESSION_STORAGE_REMOVE_ITEM,
+        payload: {
+            sessionKey: AGIFBOOKING_USER_KEY,
+            onSuccess: () => setIsSignedin(false)
         }
     };
 };
